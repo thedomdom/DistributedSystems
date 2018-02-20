36f2c97906f660f684d3ac5aa8088cad95f98e14
thedomdom <thedomdom96@gmail.com>
2018-02-20 16:05:54 +0100

    Implemented TimeService methods

--- src/timeservicermi/TimeService.java
+++ src/timeservicermi/TimeService.java
@@ -5,14 +5,56 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Date;
+import java.util.Vector;
 
 public class TimeService extends UnicastRemoteObject implements TimeService_Interface {
+
+    private Vector<Event> events;
 
     private TimeService() throws RemoteException {
     }
 
     public Date getDateAndTime() {
         return new Date();
+    }
+
+    @Override
+    public void addEvent(Event e) throws RemoteException {
+        events.add(e);
+    }
+
+    @Override
+    public Vector<Event> getAllEvents() throws RemoteException {
+        return events;
+    }
+
+    @Override
+    public Event getNextEvent() throws RemoteException {
+        Event next = null;
+        for (int i = 0; i < events.size(); i++) {
+            Event event = events.elementAt(i);
+            if (event.getDate().after(new Date())) {
+                if (next == null) {
+                    next = event;
+                }
+                if (event.getDate().before(next.getDate())) {
+                    next = event;
+                }
+            }
+        }
+        return next;
+    }
+
+    @Override
+    public Vector<Event> getFutureEvents() throws RemoteException {
+        Vector<Event> futureEvents = new Vector<>();
+        for (int i = 0; i < events.size(); i++) {
+            Event event = events.elementAt(i);
+            if (event.getDate().after(new Date())) {
+                futureEvents.add(event);
+            }
+        }
+        return futureEvents;
     }
 
     public static void main(String[] args) throws Exception {
--- src/timeservicermi/TimeService_Interface.java
+++ src/timeservicermi/TimeService_Interface.java
@@ -3,9 +3,17 @@ package timeservicermi;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.Date;
+import java.util.Vector;
 
-public interface TimeService_Interface extends Remote{
+public interface TimeService_Interface extends Remote {
 
     Date getDateAndTime() throws RemoteException;
 
+    void addEvent(Event e) throws RemoteException;
+
+    Vector<Event> getAllEvents() throws RemoteException;
+
+    Event getNextEvent() throws RemoteException;
+
+    Vector<Event> getFutureEvents() throws RemoteException;
 }
