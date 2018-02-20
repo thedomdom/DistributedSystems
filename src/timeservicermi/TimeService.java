2889400b30b39d1417dee22344bb90950affb47b
thedomdom <thedomdom96@gmail.com>
2018-02-20 15:18:50 +0100

    Started Registry

--- src/timeservicermi/TimeService.java
+++ src/timeservicermi/TimeService.java
@@ -1,10 +1,21 @@
 package timeservicermi;
 
+import java.rmi.Remote;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.Date;
 
-public class TimeService {
+public class TimeService implements Remote {
 
     Date getDateAndTime() {
         return new Date();
     }
+
+    public static void main(String[] args) throws Exception {
+        Registry registry = LocateRegistry.createRegistry(1099);
+        TimeService ts = new TimeService();
+        registry.bind("TimeService", ts);
+        System.out.println("RMI started on port 1099");
+    }
+
 }
